ef10c9f1dff115382215c0ab58d96e929ad47c8b
Jackson <jackson.cutler@gmail.com>
2020-04-16 13:24:49 -0700

    Add Recaptcha keys to env file (config/local_env.yml)

--- config/application.rb
+++ config/application.rb
@@ -17,3 +17,9 @@ module DoomsdayResources
     # the framework and any gems in your application.
   end
 end
+
+
+env_file = File.join(Rails.root, 'config', 'local_env.yml')
+YAML.load(File.open(env_file)).each do |key, value|
+  ENV[key.to_s] = value
+end if File.exists?(env_file)
