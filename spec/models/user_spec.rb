1a94ed78b024e00efa3bf09e449bdb82bd90fbb3
Michael Tornatta <michaeltornatta@gmail.com>
2020-04-18 16:02:08 -0700

    Added initial attribute tests to user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
+    user = User.new
+    specify{expect(user.role).to eq("organization")}
+    specify{expect(user.email).to eq("")}
+    # test for password?
 end
