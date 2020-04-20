86d8fc7d3e696a9ea1c0369e9e762ccffddf3f42
Michael Tornatta <michaeltornatta@gmail.com>
2020-04-20 12:59:52 -0700

    Added instantiation test to user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    user = User.new
-    specify{expect(user.role).to eq("organization")}
-    specify{expect(user.email).to eq("")}
-    # test for password?
+    let(:user) { User.new }
+
+    # instantiation test
+	it 'exists' do
+		organization
+	end
+
+	# attribute tests
+	it 'has a status' do
+		expect(organization).to respond_to(:status)
+	end
 end
