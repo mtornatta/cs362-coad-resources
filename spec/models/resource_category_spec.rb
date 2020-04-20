9f28c41ee18904a48367f63d43e75654b9675540
Jackson <jackson.cutler@gmail.com>
2020-04-20 12:40:06 -0700

    Stub out attr test for resource_category.

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 	
+	let(:resourceCategory) { ResourceCategory.new }
+
 	# instantiation test
 	it 'exists' do
 		ResourceCategory.new
+	end
+
+	# attribute tests
+	it 'has a <something>' do
 	end
 	
 end
