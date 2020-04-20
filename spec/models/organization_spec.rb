a417c52d7d0bf9641c849afdf3f8df18cd51471e
Jackson <jackson.cutler@gmail.com>
2020-04-20 12:39:42 -0700

    Create first attr test for organization.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 	
+	let(:organization) { Organization.new }
+
 	# instantiation test
 	it 'exists' do
-		Organization.new
+		organization
+	end
+
+	# attribute tests
+	it 'has a status' do
+		expect(organization).to respond_to(:status)
 	end
 
 end
