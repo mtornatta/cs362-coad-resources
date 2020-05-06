89b0e8a5bb5bed528128418f8c6ae5c57724eea5
Jackson <jackson.cutler@gmail.com>
2020-05-06 12:16:48 -0700

    User Spec: add some enum tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    let(:user) { User.new }
 
-    # instantiation test
+    #let(:user) { User.new }
+    let(:user) { FactoryBot.build(:user) }
+
+    #instantiation test
 	it 'exists' do
 		user
 	end
 
-	# attribute tests
+	#attribute tests
 	it 'has an email' do
 		expect(user).to respond_to(:email)
 	end
+
+
+	describe 'validation tests' do
+
+		it 'validates presence of an email' do
+			expect(user).to validate_presence_of(:email)
+		end
+
+		it 'validates length of email' do
+			expect(user).to validate_length_of(:email).is_at_least(1).is_at_most(255)
+		end
+
+		it 'validates email is case insensitive' do
+			expect(user).to validate_uniqueness_of(:email).case_insensitive
+		end
+
+	end
+
+	describe 'user role tests' do
+
+		it 'assigns organization role by default' do
+			expect(user.role).to eq('organization')
+		end
+
+		it 'can assign admin role to user' do
+			user = FactoryBot.build(:user, role: 'admin')
+			expect(user.role).to eq('admin')
+		end
+
+	end
+
+
+
 end
