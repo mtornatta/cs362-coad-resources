8b8b7172ecad9698351df6ea5a80deab91449e12
Jackson <jackson.cutler@gmail.com>
2020-05-06 09:43:07 -0700

    Region Spec: add some validation tests.

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 	#let(:region) { Region.new }
-	let(:region) {FactoryBot.build(:region)}
+	let(:region) { FactoryBot.build(:region) }
 
     # instantiation test
 	it 'exists' do
@@ -13,4 +13,9 @@ RSpec.describe Region, type: :model do
 	it 'has a name' do
 		expect(region).to respond_to(:name)
 	end
+
+	it 'validates presence of name' do
+		expect(region).to validate_presence_of(:name)
+	end
+
 end
