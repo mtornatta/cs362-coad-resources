9b76606fbfb30cff7c050424c7d3e9669778d13c
Jackson <jackson.cutler@gmail.com>
2020-05-14 16:09:00 -0700

    Region Spec: Add association test.

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,21 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Region, type: :model do
-  #let(:region) { Region.new }
-  let(:region) { FactoryBot.build(:region) }
 
-    # instantiation test
-  it 'exists' do
-    region
+  let(:region) { build(:region) }
+
+
+  describe 'Validation Tests' do
+
+    it 'validates presence of name' do
+      expect(region).to validate_presence_of(:name)
+    end
+
+    it 'validates length of name' do
+      expect(region).to validate_length_of(:name).is_at_least(1).is_at_most(255)
+    end
+
+    it 'validates uniqueness of name (case insensitive)' do
+      expect(region).to validate_uniqueness_of(:name).case_insensitive
+    end
+
   end
 
-  # attribute tests
-  it 'has a name' do
-    expect(region).to respond_to(:name)
+
+  describe 'Assiciation Tests' do
+
+    it 'has many tickets' do
+      expect(region).to have_many(:tickets)
+    end
+
   end
 
-  it 'validates presence of name' do
-    expect(region).to validate_presence_of(:name)
-  end
 
 end
