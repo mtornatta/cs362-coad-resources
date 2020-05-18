50f523ecf58591889823532b45f172c119985b93
Jackson <jackson.cutler@gmail.com>
2020-05-17 18:59:42 -0700

    Region: organize spec.

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -6,27 +6,37 @@ RSpec.describe Region, type: :model do
   let(:region) { build(:region) }
 
 
+  describe 'Assiciation Tests' do
+
+    it 'has many tickets' do
+      expect(region).to have_many(:tickets)
+    end
+
+  end
+
+
   describe 'Validation Tests' do
 
-    it 'validates presence of name' do
+    it 'must have a name' do
       expect(region).to validate_presence_of(:name)
     end
 
-    it 'validates length of name' do
+    it 'has a valid length name' do
       expect(region).to validate_length_of(:name).is_at_least(1).is_at_most(255)
     end
 
-    it 'validates uniqueness of name (case insensitive)' do
+    it 'has a unique name (case insensitive)' do
       expect(region).to validate_uniqueness_of(:name).case_insensitive
     end
 
   end
 
 
-  describe 'Assiciation Tests' do
+  describe 'Method Tests' do
 
-    it 'has many tickets' do
-      expect(region).to have_many(:tickets)
+    #to_s
+    it 'is represented by its name' do
+      expect(region.to_s).to eq(region.name)
     end
 
   end
