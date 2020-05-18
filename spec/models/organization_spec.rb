6feaa57d149f008ee05bb983bf49210a763e12c3
Jackson <jackson.cutler@gmail.com>
2020-05-17 17:56:24 -0700

    Organization: update let blocks.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,82 +2,85 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 	
+  # Stub Build
   let(:organization) { build(:organization) }
 
 
   describe 'Attribute Tests' do
 
-  	it 'has transportation' do
+  	it 'can have transportation' do
   	  expect(organization).to respond_to(:transportation)
   	end
 
-    it 'has a description' do
+    it 'can have a description' do
       expect(organization).to respond_to(:description)
     end
 
   end
 
 
-  describe 'Presence-of Validation Tests' do
+  describe 'Validation Tests' do
 
-  	it 'validates presence of email' do
-  	  expect(organization).to validate_presence_of(:email)
-  	end
+    describe 'Presence-of' do
 
-  	it 'validates presence of name' do
-  	  expect(organization).to validate_presence_of(:name)
+    	it 'must have a email' do
+    	  expect(organization).to validate_presence_of(:email)
+    	end
+
+    	it 'must have a name' do
+    	  expect(organization).to validate_presence_of(:name)
+      end
+
+    	it 'must have a phone' do
+    	  expect(organization).to validate_presence_of(:phone)
+    	end
+
+    	it 'must have a status' do
+    	  expect(organization).to validate_presence_of(:status)
+    	end
+
+    	it 'must have a primary name' do
+    	  expect(organization).to validate_presence_of(:primary_name)
+    	end
+
+    	it 'must have a secondary name' do
+    	  expect(organization).to validate_presence_of(:secondary_name)
+    	end
+    	
+    	it 'must have a secondary phone' do
+    	  expect(organization).to validate_presence_of(:secondary_phone)
+    	end
+
     end
 
-  	it 'validates presence of phone' do
-  	  expect(organization).to validate_presence_of(:phone)
-  	end
+    describe 'Length' do
 
-  	it 'validates presence of status' do
-  	  expect(organization).to validate_presence_of(:status)
-  	end
+      it 'has a valid sized email' do
+        expect(organization).to validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create)
+      end
 
-  	it 'validates presence of primary name' do
-  	  expect(organization).to validate_presence_of(:primary_name)
-  	end
+      it 'has a valid sized name' do
+        expect(organization).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+      end
 
-  	it 'validates presence of secondary name' do
-  	  expect(organization).to validate_presence_of(:secondary_name)
-  	end
+      it 'has a valid must have asized description' do
+        expect(organization).to validate_length_of(:description).is_at_most(1020).on(:create)
+      end
+
+    end
+
+    describe 'Uniqueness' do
+
+      it 'has a unique email' do
+        expect(organization).to validate_uniqueness_of(:email).case_insensitive
+      end
+
+      it 'has a unique name' do
+        expect(organization).to validate_uniqueness_of(:name).case_insensitive
+      end
+
+    end
   	
-  	it 'validates presence of secondary phone' do
-  	  expect(organization).to validate_presence_of(:secondary_phone)
-  	end
-  	
-  end
-
-
-  describe 'Length Validation Tests' do
-
-  	it 'validates length of email' do
-      expect(organization).to validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create)
-  	end
-
-    it 'validates length of name' do
-      expect(organization).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
-    end
-
-    it 'validates length of description' do
-      expect(organization).to validate_length_of(:description).is_at_most(1020).on(:create)
-    end
-  	
-  end
-
-
-  describe 'Uniqueness Validation Tests' do
-
-    it 'validates uniqueness of email' do
-      expect(organization).to validate_uniqueness_of(:email).case_insensitive
-    end
-
-    it 'validates uniqueness of name' do
-      expect(organization).to validate_uniqueness_of(:name).case_insensitive
-    end
-
   end
 
 
