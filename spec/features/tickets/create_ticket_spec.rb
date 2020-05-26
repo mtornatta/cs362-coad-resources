1f29db6b8308220e43613e064700b7c9501abd9b
Jackson <jackson.cutler@gmail.com>
2020-05-26 08:59:38 -0700

    Stub out ticket feature.

--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Ticket', type: :feature do
+	visit root_path
+  click_link 'Get Help'
+  fill_in 'Full Name', with: 'fake name'
+  fill_in 'Phone Number', with: 'etc.'
+  # select 'Bend', from: 'Region'
+  # select ''
 
 end
