23cc98bd14992909866ee19edf226e06ec2dc2f1
Jackson <jackson.cutler@gmail.com>
2020-05-28 08:36:50 -0700

    Create Ticket: Comment unfinished spec.

--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Ticket', type: :feature do
-	visit root_path
-  click_link 'Get Help'
-  fill_in 'Full Name', with: 'fake name'
-  fill_in 'Phone Number', with: 'etc.'
+  # visit root_path
+  # click_link 'Get Help'
+  # fill_in 'Full Name', with: 'fake name'
+  # fill_in 'Phone Number', with: 'etc.'
   # select 'Bend', from: 'Region'
   # select ''
 
