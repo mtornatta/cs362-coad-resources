af5f46bdb5369364bbf521db99ba45b8ae5ee6bf
Jackson <jackson.cutler@gmail.com>
2020-05-29 14:16:03 -0700

    Regions Controller: Create method.

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
 
+  context 'Admin User' do
+    describe 'POST #create' do
+      let(:user) { create(:user, :admin) }
+      before do
+        user.confirm
+        sign_in(user)
+      end
+
+      it 'succeeds' do
+        expect(
+          post(
+            :create, 
+            params: attributes_for(:region) 
+          )
+        ).to redirect_to(regions_path)
+      end
+    end
+  end
+
 end
