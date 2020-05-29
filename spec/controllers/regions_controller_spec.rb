6249dce372c38808836f4d6f30292fb769a4d9fe
Michael Tornatta <michaeltornatta@gmail.com>
2020-05-29 15:38:24 -0700

    Working on regions controller

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -14,11 +14,44 @@ RSpec.describe RegionsController, type: :controller do
         expect(
           post(
             :create, 
-            params: attributes_for(:region) 
+            params: {
+              region: attributes_for(:region)
+            } 
           )
         ).to redirect_to(regions_path)
       end
     end
+
+    describe 'GET #index' do
+      let(:region) { create(:region) }
+      before do
+        region
+      end
+      it 'succeeds' do
+        expect(controller.index).to include(region)
+      end
+    end
+    
+    describe 'PATCH #update' do
+      let(:region) { create(:region) }
+      let(:user) { create(:user, :admin) }
+      before do
+        user.confirm
+        sign_in(user)
+      end
+
+      it 'succeeds' do
+        expect(
+          patch(
+            :create, 
+            params: {
+              region: attributes_for(:region)
+            } 
+          )
+        ).to redirect_to("/regions/#{region.id}")
+      end
+    end
+
   end
 
 end
