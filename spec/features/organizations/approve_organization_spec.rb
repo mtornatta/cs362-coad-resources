a523cef50511bf582aae46c9b30801224e440f9e
Michael Tornatta <michaeltornatta@gmail.com>
2020-05-30 14:41:22 -0700

    Worked on approve organization feature

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
-
+  let(:organization) {create(:organization, :submitted)}
+  let(:admin) {create(:user, :admin)}
+  
+  describe 'as an admin' do
+    before do
+      admin.confirm
+      sign_in(admin)
+    end
+    it 'approves an organization successfully' do
+        visit("/organizations/#{organization.id}")
+        click_button('Approve')
+        expect(page).to have_content("#{organization.name}")
+    end
+  end
 end
