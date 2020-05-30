16c289d0c443c4a59ebab9b77dc43568b0fbecbc
Jackson <jackson.cutler@gmail.com>
2020-05-30 09:05:21 -0700

    Regions Controller: Update and Show tests complete.

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -3,14 +3,38 @@ require 'rails_helper'
 RSpec.describe RegionsController, type: :controller do
 
   context 'Admin User' do
+    let(:region) { create(:region) }
+
+    describe 'GET #index' do
+
+      it 'returns list of all regions' do
+        expect(controller.index).to include(region)
+      end
+    end
+
+    describe 'GET #show' do
+      let(:region) { create(:region) }
+
+      it 'finds the specified region' do
+        get :show, params: { id: region.id }
+        expect(controller.show).to eq(region)
+      end
+    end
+
+
+    describe 'GET #new' do
+    end
+
+
     describe 'POST #create' do
       let(:user) { create(:user, :admin) }
+
       before do
         user.confirm
         sign_in(user)
       end
 
-      it 'succeeds' do
+      it 'creates a region' do
         expect(
           post(
             :create, 
@@ -22,38 +46,34 @@ RSpec.describe RegionsController, type: :controller do
       end
     end
 
-    describe 'GET #index' do
-      let(:region) { create(:region) }
-      before do
-        region
-      end
-      it 'succeeds' do
-        expect(controller.index).to include(region)
-      end
+
+    describe 'GET #edit' do
     end
+
     
     describe 'PATCH #update' do
-      let(:region) { create(:region) }
       let(:user) { create(:user, :admin) }
+
       before do
         user.confirm
         sign_in(user)
       end
 
-      it 'succeeds' do
+      it 'confirms updates to a region' do
         expect(
-          post(
-            :create, 
+          patch(
+            :update, 
             params: {
+              id: region.id,
               region: attributes_for(:region)
             }
           )
-          patch(
-            :update, 
-            params: attributes_for(:region)
-          )
         ).to redirect_to("/regions/#{region.id}")
       end
+    end
+
+
+    describe 'DELETE #destroy' do
     end
 
   end
