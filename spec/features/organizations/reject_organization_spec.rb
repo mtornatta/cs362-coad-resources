cba920476695ab3f3301f6db0f3b397ef8dd84fd
Jackson <jackson.cutler@gmail.com>
2020-05-30 19:53:11 -0700

    Reject Org: Copy approve org and change button name.

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
-
+  let(:organization) { create(:organization, :submitted) }
+  let(:admin) { create(:user, :admin) }
+  
+  context 'as an admin' do
+    before do
+      admin.confirm
+      log_in_as(admin)
+      organization.save
+    end
+    it 'approves an organization successfully' do
+        visit organization_path(organization.id)
+        click_on('Reject')
+        expect(page).to have_content("#{organization.name} has been rejected.")
+    end
+  end
 end
