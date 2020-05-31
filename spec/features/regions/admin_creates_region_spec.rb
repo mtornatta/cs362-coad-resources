a40a174c9cb59e00b50226dd64504eb96aa85780
Jackson <jackson.cutler@gmail.com>
2020-05-31 13:03:53 -0700

    Create Region: Change visit call to go directly to creation page.

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
+  
   let(:admin) { create(:user, :admin) }
 
   context 'as an admin' do
@@ -10,20 +11,18 @@ RSpec.describe 'Creating a Region', type: :feature do
     end
 
     it 'displays success message' do
-      visit regions_path
-      click_link 'Add Region'
+      visit new_region_path
       fill_in 'Name', with: 'Feature Test Region'
       click_button 'Add Region'
       expect(page).to have_content('success')
     end
 
     it 'displays failure message when name is blank' do
-      visit regions_path
-      click_link 'Add Region'
+      visit new_region_path
       fill_in 'Name', with: ''
       click_button 'Add Region'
       expect(page).to have_content('error')
     end
   end
-  
+
 end
