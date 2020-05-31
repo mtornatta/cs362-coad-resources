4d421456c3ce22e4d2286014749a900e98c2d317
Jackson <jackson.cutler@gmail.com>
2020-05-31 11:18:46 -0700

    Create Region: Test happy path and sad path.

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
-    
+  let(:admin) { create(:user, :admin) }
+
+  context 'as an admin' do
+    before do
+      admin.confirm
+      log_in_as(admin)
+    end
+
+    it 'displays success message' do
+      visit regions_path
+      click_link 'Add Region'
+      fill_in 'Name', with: 'Feature Test Region'
+      click_button 'Add Region'
+      expect(page).to have_content('success')
+    end
+
+    it 'displays failure message when name is blank' do
+      visit regions_path
+      click_link 'Add Region'
+      fill_in 'Name', with: ''
+      click_button 'Add Region'
+      expect(page).to have_content('error')
+    end
+  end
+  
 end
