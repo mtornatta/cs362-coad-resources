6ef87cd65300031782d5ec68ef2ee0b926d22780
Jackson <jackson.cutler@gmail.com>
2020-05-31 15:11:48 -0700

    Dashboard Helper: Test 4 user scenarios.

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -13,15 +13,32 @@ require 'rails_helper'
 RSpec.describe DashboardHelper, type: :helper do
 
   it 'returns admin dashboard' do
+    admin = double()
+    admin.stub(:admin?).and_return(true)
+    expect(dashboard_for(admin)).to eq('admin_dashboard')
   end
 
   it 'returns org submitted dashboard' do
+    user = double()
+    user.stub(:admin?).and_return(false)
+    user.stub_chain(:organization, :submitted?).and_return(true)
+    expect(dashboard_for(user)).to eq('organization_submitted_dashboard')
   end
 
   it 'returns org approved dashboard' do
+    user = double()
+    user.stub(:admin?).and_return(false)
+    user.stub_chain(:organization, :submitted?).and_return(false)
+    user.stub_chain(:organization, :approved?).and_return(true)
+    expect(dashboard_for(user)).to eq('organization_approved_dashboard')
   end
 
   it 'returns create application dashboard' do
+    user = double()
+    user.stub(:admin?).and_return(false)
+    user.stub_chain(:organization, :submitted?).and_return(false)
+    user.stub_chain(:organization, :approved?).and_return(false)
+    expect(dashboard_for(user)).to eq('create_application_dashboard')
   end
 
 end
