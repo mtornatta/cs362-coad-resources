9da944dc5c5ae4b2fd7a59790cc3d87e3c59070f
Jackson <jackson.cutler@gmail.com>
2020-06-01 13:42:13 -0700

    Dashboard Helper: Update old syntax.

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -14,30 +14,30 @@ RSpec.describe DashboardHelper, type: :helper do
 
   it 'returns admin dashboard' do
     admin = double()
-    admin.stub(:admin?).and_return(true)
+    allow(admin).to receive(:admin?).and_return(true)
     expect(dashboard_for(admin)).to eq('admin_dashboard')
   end
 
   it 'returns org submitted dashboard' do
     user = double()
-    user.stub(:admin?).and_return(false)
-    user.stub_chain(:organization, :submitted?).and_return(true)
+    allow(user).to receive(:admin?).and_return(false)
+    allow(user).to receive_message_chain(:organization, :submitted?).and_return(true)
     expect(dashboard_for(user)).to eq('organization_submitted_dashboard')
   end
 
   it 'returns org approved dashboard' do
     user = double()
-    user.stub(:admin?).and_return(false)
-    user.stub_chain(:organization, :submitted?).and_return(false)
-    user.stub_chain(:organization, :approved?).and_return(true)
+    allow(user).to receive(:admin?).and_return(false)
+    allow(user).to receive_message_chain(:organization, :submitted?).and_return(false)
+    allow(user).to receive_message_chain(:organization, :approved?).and_return(true)
     expect(dashboard_for(user)).to eq('organization_approved_dashboard')
   end
 
   it 'returns create application dashboard' do
     user = double()
-    user.stub(:admin?).and_return(false)
-    user.stub_chain(:organization, :submitted?).and_return(false)
-    user.stub_chain(:organization, :approved?).and_return(false)
+    allow(user).to receive(:admin?).and_return(false)
+    allow(user).to receive_message_chain(:organization, :submitted?).and_return(false)
+    allow(user).to receive_message_chain(:organization, :approved?).and_return(false)
     expect(dashboard_for(user)).to eq('create_application_dashboard')
   end
 
