952766880de560c36fa22bf39a4eb495849e4e1f
Jackson <jackson.cutler@gmail.com>
2020-06-01 10:18:26 -0700

    Res Cat Controller: Complete tests for 3 types of users.

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe ResourceCategoriesController, type: :controller do
 
   context 'As an admin' do
 
-    let(:recource_category) { create(:resource_category) }
+    let(:resource_category) { create(:resource_category) }
     let(:admin) { create(:user, :admin) }
 
     describe 'GET #index' do
@@ -18,16 +18,16 @@ RSpec.describe ResourceCategoriesController, type: :controller do
     end
 
     # uncomment if implemented:
-    # 
-    # describe 'GET #show' do
-    #   before do
-    #     admin.confirm
-    #     sign_in(admin)
-    #   end
-    #   it 'is successful' do
-    #     expect(get :show).to be_successful
-    #   end
-    # end
+      # 
+        # describe 'GET #show' do
+        #   before do
+        #     admin.confirm
+        #     sign_in(admin)
+        #   end
+        #   it 'is successful' do
+        #     expect(get :show).to be_successful
+        #   end
+        # end
 
     describe 'GET #new' do
       before do
@@ -35,6 +35,7 @@ RSpec.describe ResourceCategoriesController, type: :controller do
         sign_in(admin)
       end
       it 'is successful' do
+        expect(get :new).to be_successful
       end
     end
 
@@ -43,28 +44,45 @@ RSpec.describe ResourceCategoriesController, type: :controller do
         admin.confirm
         sign_in(admin)
       end
-      it 'is successful' do
+      it 'redirects to recource categories' do
+        expect(
+          post(
+            :create,
+            params: {
+              resource_category: attributes_for(:resource_category)
+            }
+          )
+        ).to redirect_to(resource_categories_path)
       end
     end
 
     # uncomment if implemented:
-    # 
-    # describe 'GET #edit' do
-    #   before do
-    #     admin.confirm
-    #     sign_in(admin)
-    #   end
-    #   it 'is successful' do
-    #     expect(get :edit).to be_successful
-    #   end
-    # end
+      # 
+        # describe 'GET #edit' do
+        #   before do
+        #     admin.confirm
+        #     sign_in(admin)
+        #   end
+        #   it 'is successful' do
+        #     expect(get :edit).to be_successful
+        #   end
+        # end
 
     describe 'PATCH #update' do
       before do
         admin.confirm
         sign_in(admin)
       end
-      it 'is successful' do
+      it 'redirects to the updated resource category' do
+        expect(
+          patch(
+            :update,
+            params: {
+              id: resource_category.id,
+              resource_category: attributes_for(:resource_category)
+            }
+          )
+        ).to redirect_to(resource_category_path(resource_category))
       end
     end
 
@@ -73,7 +91,13 @@ RSpec.describe ResourceCategoriesController, type: :controller do
         admin.confirm
         sign_in(admin)
       end
-      it 'is successful' do
+      it 'redirects to activated resource category' do
+        expect(
+          patch(
+            :activate,
+            params: { id: resource_category.id }
+          )
+        ).to redirect_to(resource_category_path(resource_category))
       end
     end
 
@@ -82,7 +106,13 @@ RSpec.describe ResourceCategoriesController, type: :controller do
         admin.confirm
         sign_in(admin)
       end
-      it 'is successful' do
+      it 'redirects to deactivated resource category' do
+        expect(
+          patch(
+            :deactivate,
+            params: { id: resource_category.id }
+          )
+        ).to redirect_to(resource_category_path(resource_category))
       end
     end
 
@@ -91,7 +121,13 @@ RSpec.describe ResourceCategoriesController, type: :controller do
         admin.confirm
         sign_in(admin)
       end
-      it 'is successful' do
+      it 'redirects to resource categories' do
+        expect(
+          patch(
+            :destroy,
+            params: { id: resource_category.id }
+          )
+        ).to redirect_to(resource_categories_path)
       end
     end
 
@@ -101,7 +137,7 @@ RSpec.describe ResourceCategoriesController, type: :controller do
 
 context 'As a registered user' do
 
-    let(:recource_category) { create(:resource_category) }
+    let(:resource_category) { create(:resource_category) }
     let(:user) { create(:user) }
 
     describe 'GET #index' do
@@ -132,6 +168,7 @@ context 'As a registered user' do
         sign_in(user)
       end
       it 'is not successful' do
+        expect(get :new).not_to be_successful
       end
     end
 
@@ -140,7 +177,15 @@ context 'As a registered user' do
         user.confirm
         sign_in(user)
       end
-      it 'is not successful' do
+      it 'redirects to dashboard' do
+        expect(
+          post(
+            :create,
+            params: {
+              resource_category: attributes_for(:resource_category)
+            }
+          )
+        ).to redirect_to(dashboard_path)
       end
     end
 
@@ -161,8 +206,17 @@ context 'As a registered user' do
         user.confirm
         sign_in(user)
       end
-      it 'is not successful' do
-      end
+      it 'redirects to dashboard' do
+        expect(
+          patch(
+            :update,
+            params: {
+              id: resource_category.id,
+              resource_category: attributes_for(:resource_category)
+            }
+          )
+        ).to redirect_to(dashboard_path)
+      end 
     end
 
     describe 'PATCH #activate' do
@@ -170,7 +224,13 @@ context 'As a registered user' do
         user.confirm
         sign_in(user)
       end
-      it 'is not successful' do
+      it 'redirects to dashboard' do
+        expect(
+          patch(
+            :activate,
+            params: { id: resource_category.id }
+          )
+        ).to redirect_to(dashboard_path)
       end
     end
 
@@ -179,7 +239,13 @@ context 'As a registered user' do
         user.confirm
         sign_in(user)
       end
-      it 'is not successful' do
+      it 'redirects to dashboard' do
+        expect(
+          patch(
+            :deactivate,
+            params: { id: resource_category.id }
+          )
+        ).to redirect_to(dashboard_path)
       end
     end
 
@@ -188,7 +254,13 @@ context 'As a registered user' do
         user.confirm
         sign_in(user)
       end
-      it 'is not successful' do
+      it 'redirects to dashboard' do
+        expect(
+          patch(
+            :destroy,
+            params: { id: resource_category.id }
+          )
+        ).to redirect_to(dashboard_path)
       end
     end
 
@@ -198,7 +270,7 @@ context 'As a registered user' do
 
   context 'As a public user' do
 
-    let(:recource_category) { create(:resource_category) }
+    let(:resource_category) { create(:resource_category) }
 
     describe 'GET #index' do
       it 'is not successful' do
@@ -216,11 +288,20 @@ context 'As a registered user' do
 
     describe 'GET #new' do
       it 'is not successful' do
+        expect(get :new).not_to be_successful
       end
     end
 
     describe 'POST #create' do
-      it 'is not successful' do
+      it 'redirects to dashboard' do
+        expect(
+          post(
+            :create,
+            params: {
+              resource_category: attributes_for(:resource_category)
+            }
+          )
+        ).to redirect_to(user_session_path)
       end
     end
 
@@ -233,22 +314,49 @@ context 'As a registered user' do
     # end
 
     describe 'PATCH #update' do
-      it 'is not successful' do
-      end
+      it 'redirects to login page' do
+        expect(
+          patch(
+            :update,
+            params: {
+              id: resource_category.id,
+              resource_category: attributes_for(:resource_category)
+            }
+          )
+        ).to redirect_to(user_session_path)
+      end 
     end
 
     describe 'PATCH #activate' do
-      it 'is not successful' do
+      it 'redirects to login page' do
+        expect(
+          patch(
+            :activate,
+            params: { id: resource_category.id }
+          )
+        ).to redirect_to(user_session_path)
       end
     end
 
     describe 'PATCH #deactivate' do
-      it 'is not successful' do
+      it 'redirects to login page' do
+        expect(
+          patch(
+            :deactivate,
+            params: { id: resource_category.id }
+          )
+        ).to redirect_to(user_session_path)
       end
     end
 
     describe 'DELETE #destroy' do
-      it 'is not successful' do
+      it 'redirects to login page' do
+        expect(
+          patch(
+            :destroy,
+            params: { id: resource_category.id }
+          )
+        ).to redirect_to(user_session_path)
       end
     end
 
