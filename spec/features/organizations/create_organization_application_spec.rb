e7f420b2b7b33088230b531d1f80423c071f4c91
Michael Tornatta <michaeltornatta@gmail.com>
2020-06-01 10:40:50 -0700

    Worked on organization feature tests and resource_category test

--- spec/features/organizations/create_organization_application_spec.rb
+++ spec/features/organizations/create_organization_application_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating an Organization Application', type: :feature do
+#   let(:user) {create(:user)}
+  
+#   describe 'as a user' do
+#     before do
+#       user.confirm
+#       log_in_as(user)
+#     end
+#     it 'get to organization application page successfully' do
+#         visit organization_path("dashboard")
+#         click_on('Create Application')
+#         expect(page).to have_content("Partner Organization Application")
+#     end
 
+#     it 'fills out and submits an application page successfully' do
+#         visit organization_path("dashboard")
+#         click_on('Create Application')
+#         choose('organization[agreement_one]')
+#         choose('organization[agreement_three]')
+#         choose('organization[agreement_four]')
+#         choose('organization[agreement_five]')
+#         choose('organization[agreement_six]')
+#         choose('organization[agreement_seven]')
+#         choose('organization[agreement_eight]')
+#         fill_in('What is your name? (Last, First)', with: 'Test Name')
+#         fill_in('Organization Name', with: 'Test Organization')
+#         fill_in('What is your title? (if applicable)', with: 'Test Title')
+#         fill_in('What is your direct phone number? Cell phone is best.', with: '1231234567')
+#         fill_in('Who may we contact regarding your organization\'s application if we are unable to reach you?', with: 'Test Contact')
+#         fill_in('What is a good secondary phone number we may reach your organization at?', with: '7654321321')
+#         fill_in('What is your Organization\'s email?', with: 'fake@fake.com')
+#         choose('organization_transportation_no')
+#         click_on('Apply')
+#         expect(page).to have_content('Application Pending')
+#     end
+#    end
 end
--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Resource Category', type: :feature do
-
+    # let(:region) { create(:region) }
+    # let(:admin) { create(:user, :admin) }
+    
+    # context 'as an admin' do
+    #   before do
+    #     admin.confirm
+    #     log_in_as(admin)
+    #   end
+  
+    #   it 'approves an organization successfully' do
+    #       visit organization_path("/regions")
+    #       click_on('Approve')
+    #       expect(page).to have_content('approved')
+    #   end
+    # end
 end
