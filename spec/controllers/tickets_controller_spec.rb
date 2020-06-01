e1aac34f0e5d7d5c6e00abdb3fe392120611cbfc
Jackson <jackson.cutler@gmail.com>
2020-06-01 16:33:57 -0700

    Ticket Controller: Stub out first test.

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,4 +2,35 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
 
+  context 'As an admin' do
+
+    let(:ticket) { create(:ticket) }
+    let(:admin) { create(:user, :admin) }
+    
+    describe 'GET #new' do
+      before do
+        admin.confirm
+        sign_in(admin)
+      end
+      it 'is successful' do
+        expect(get :new).to be_successful
+      end
+    end
+
+  end
+
+  context 'As a registered user' do
+
+    let(:ticket) { create(:ticket) }
+    let(:user) { create(:user) }
+
+  end
+
+  context 'As a public user' do
+
+    let(:ticket) { create(:ticket) }
+
+  end
+
+
 end
