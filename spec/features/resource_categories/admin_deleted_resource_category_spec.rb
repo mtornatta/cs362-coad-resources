207986ff0bd6c9f236f3003b412861f1feca2205
Michael Tornatta <michaeltornatta@gmail.com>
2020-06-02 01:54:13 -0700

    Progress on resource category feature test, still failing

--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Resource Category', type: :feature do
-#     it 'successfully' do
-#         resource_category = create(:resource_category)
-#         admin = create(:user,:admin)
-#         admin.confirm
-#         log_in_as(admin)
-#         visit resource_categories_path("/resource_categories/#{resource_category.id}")
-#         byebug
-#         click_on('Delete')
-#         accept_confirm do
-#             click_link 'OK'
-#         end
-#         expect(current_path).to eq(dashboard_path)
-#     end
+    it 'successfully' do
+        resource_category = create(:resource_category)
+        admin = create(:user,:admin)
+        admin.confirm
+        log_in_as(admin)
+        visit resource_categories_path(resource_category.id)
+        byebug
+        click_on('Delete')
+        accept_confirm do
+            click_link 'OK'
+        end
+        expect(current_path).to eq(dashboard_path)
+    end
 end
