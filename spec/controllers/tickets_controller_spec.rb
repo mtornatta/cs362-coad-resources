3ac51d48e935f158714879e3f482371aa77f43aa
Jackson <jackson.cutler@gmail.com>
2020-06-01 21:56:25 -0700

    Ticket Controller: Write tests for capture method.

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -17,12 +17,46 @@ RSpec.describe TicketsController, type: :controller do
       end
     end
 
+    describe 'POST #capture' do
+      before do
+        admin.confirm
+        sign_in(admin)
+      end
+      it 'redirects to dashboard' do
+        expect(
+          post(
+            :capture,
+            params: { id: ticket.id }
+          )
+        ).to redirect_to(dashboard_path)
+      end
+    end
+
   end
 
   context 'As a registered user' do
 
     let(:ticket) { create(:ticket) }
     let(:user) { create(:user) }
+
+    describe 'POST #capture' do
+      before do
+        user.confirm
+        sign_in(user)
+      end
+      it 'redirects to dashboard' do
+        organization = create(:organization, :approved)
+        organization.save
+        user.organization = organization
+        user.save
+        expect(
+          post(
+            :capture,
+            params: { id: ticket.id }
+          )
+        ).to redirect_to(dashboard_path << '#tickets:open')
+      end
+    end
 
   end
 
@@ -31,6 +65,5 @@ RSpec.describe TicketsController, type: :controller do
     let(:ticket) { create(:ticket) }
 
   end
-
 
 end
