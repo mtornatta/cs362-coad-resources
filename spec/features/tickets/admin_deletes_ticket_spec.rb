f65c0c81403b5bf69aaedeadf8e042f762ebe3a7
Michael Tornatta <michaeltornatta@gmail.com>
2020-06-01 22:58:18 -0700

    Progress on the rest of the feature tests

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Ticket', type: :feature do
-
+    it 'succeeds as admin' do
+        organization = create(:organization, :approved)
+        admin = create(:user, :admin)
+        admin.organization = organization
+        admin.save
+        admin.confirm
+        log_in_as(admin)
+        ticket = create(:ticket)
+        visit ticket_path(ticket.id)
+        click_on 'Delete'
+        expect(current_path).to eq(dashboard_path)
+    end
 end
--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
-
+    it 'succeeds as organization user' do
+        organization = create(:organization, :approved)
+        user = create(:user)
+        user.organization = organization
+        user.save
+        user.confirm
+        log_in_as(user)
+        ticket = create(:ticket)
+        visit ticket_path(ticket.id)
+        click_on 'Capture'
+        expect(current_path).to eq(dashboard_path)
+    end
 end
