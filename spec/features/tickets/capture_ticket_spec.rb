f35aa5a4f677d741352e2560bb813741b56edfac
Jackson <jackson.cutler@gmail.com>
2020-06-01 21:55:44 -0700

    Capture Ticket: No change.

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
-
+  # it 'succeeds' do
+  #   organization = create(:organization, :approved)
+  #   user = create(:user)
+  #   user.organization = organization
+  #   user.save
+  #   user.confirm
+  #   log_in_as(user)
+  #   ticket = create(:ticket)
+  #   ticket.organization = organization
+  #   ticket.save
+  #   visit ticket_path(ticket.id)
+  #   click_on 'Capture'
+  #   expect(current_path).to eq(dashboard_path)
+  # end
 end
