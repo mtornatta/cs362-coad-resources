ba38902a0a5dd82aa175b87b0f2717ef30b78858
Jackson <jackson.cutler@gmail.com>
2020-06-02 02:55:06 -0700

    Update Org: Test both types of users.

--- spec/features/organizations/update_organization_spec.rb
+++ spec/features/organizations/update_organization_spec.rb
@@ -1,31 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Updating an Organization', type: :feature do
-    # let(:organization) {create(:organization)}
 
-    # context 'As an admin' do
-    #      it 'updating an organization successfully' do
-    #          visit edit_organization_path(organization)
-    #          fill_in 'Name', with: 'New Fake Name'
-    #          click 'Save'
-    #          expect(page).to have_content('New Fake Name')
-    #      end
-    #  end
+  let(:organization) {create(:organization, :approved)}
 
-    # context 'As an org user' do
-    #     let (:organization) {create(:organization, :approved)}
-    #     let (:user) {create(:user)}
+  context 'As an admin' do
+    let(:admin) { create(:user, :admin) }
+    before do
+      admin.confirm
+      log_in_as(admin)
+    end
+     it 'updating an organization successfully' do
+       visit edit_organization_path(organization)
+       fill_in 'Name', with: 'New Fake Name'
+       click_on 'Update Resource'
+       expect(page).to have_content('New Fake Name')
+     end
+   end
 
-    #     before do
-    #         user.organization = organization
-    #         user.save
-    #     end
+  context 'As an org user' do
 
-    #     it 'updating an organization successfully' do
-    #         visit edit_organization_path(organization)
-    #         fill_in :name, with: 'New Fake Name'
-    #         click 'Save'
-    #         expect(page).to have_content('New Fake Name')
-    #     end
-    # end
+    let (:user) {create(:user)}
+
+    before do
+      user.organization = organization
+      user.confirm
+      log_in_as(user)
+    end
+    it 'updating an organization successfully' do
+      visit edit_organization_path(organization)
+      fill_in 'Name', with: 'New Fake Name'
+      click_on 'Update Resource'
+      expect(page).to have_content('New Fake Name')
+    end
+  end
 end
