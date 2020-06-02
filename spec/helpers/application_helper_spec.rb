2a5412411a41d59a3ec97e6e1b4ea91992857b87
Jackson <jackson.cutler@gmail.com>
2020-06-02 00:49:56 -0700

    Application Helper: Test 2 cases of full_title method.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
 
+  let(:base_title) { 'Disaster Resource Network' }
+
+  it 'returns base title with no parameters' do
+    expect(full_title).to eq(base_title)
+  end
+
+  it 'returns base title | page title when passed' do
+    page_title = 'Test Page Title'
+    expect(full_title(page_title)).to eq("#{page_title} | #{base_title}")
+  end
+
 end
