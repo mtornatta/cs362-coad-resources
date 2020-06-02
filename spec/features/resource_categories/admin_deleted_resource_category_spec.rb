9d6707d4afcb2d91a7bd42b9ef4ac7d481c8b6b4
Jackson <jackson.cutler@gmail.com>
2020-06-02 03:36:39 -0700

    Whitespace

--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Resource Category', type: :feature do
-    it 'successfully' do
-        resource_category = create(:resource_category)
-        admin = create(:user,:admin)
-        admin.confirm
-        log_in_as(admin)
-        visit resource_category_path(resource_category.id)
-        click_on('Delete')
-        expect(page).to have_content('deleted')
-    end
+  it 'successfully' do
+    resource_category = create(:resource_category)
+    admin = create(:user,:admin)
+    admin.confirm
+    log_in_as(admin)
+    visit resource_category_path(resource_category.id)
+    click_on('Delete')
+    expect(page).to have_content('deleted')
+  end
 end
